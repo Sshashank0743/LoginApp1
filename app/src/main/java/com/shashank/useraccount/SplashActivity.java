77ed1fc459d5d67295e5a07406423e7c831a7c20
Shashank Shukla <s.shukla9919@gmail.com>
2022-11-18 11:00:31 +0530

    Initial commit

---
+++ app/src/main/java/com/shashank/useraccount/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.shashank.useraccount;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        //title bar hide
+        getSupportActionBar().hide(); //hide the title bar
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 5000);
+
+//if you want to change your splash screen stay 5 second or more then just  edit 5000
+    }
+}
